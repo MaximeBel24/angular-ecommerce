ef9e0c11cfe7c30c4b48f5bc55725369441ed6d3
Maxime <maxime.b2494@gmail.com>
2025-01-20 21:36:44 +0100

    chapitre 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./features/navbar/navbar.component";
-import { TestComponent } from "./pages/test/test.component";
 import { ProductCardComponent } from "./shared/components/product-card/product-card.component";
+import { ProductComponent } from "./pages/product/product.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, TestComponent, ProductCardComponent],
+  imports: [NavbarComponent, ProductCardComponent, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommentsComponent } from '../../features/comments/comments.component';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommentsComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
 
+  areCommentsVisible = false;
+
+  toggleCommentsVisibility() {
+    this.areCommentsVisible = !this.areCommentsVisible;
+  }
 }
