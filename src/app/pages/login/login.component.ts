9d1c679dd64050e1994242dc8ba7d9f23ef73327
Maxime <maxime.b2494@gmail.com>
2025-01-25 16:15:25 +0100

    chapitre 30

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   private router = inject(Router);
 
-  login() {
-    this.router.navigateByUrl('/');
+  login(form:NgForm) {
+    if (form.invalid) return;
+    console.log(form.value)
+    // this.router.navigateByUrl('/');
   }
 }
