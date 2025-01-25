f42f7fc76f839359ea6db6935d96a2028829fff4
Maxime <maxime.b2494@gmail.com>
2025-01-25 19:28:25 +0100

    chapitre 31

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,37 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   private router = inject(Router);
 
-  login(form:NgForm) {
-    if (form.invalid) return;
-    console.log(form.value)
+  propEmail: FormControl = new FormControl<string>('', [
+    Validators.required,
+    Validators.email
+  ]);
+  propPassword: FormControl = new FormControl<string>('', [
+    Validators.required,
+    Validators.minLength(8)
+  ]);
+  propRemember: FormControl = new FormControl<boolean>(false);
+
+  form: FormGroup = new FormGroup({
+    email: this.propEmail,
+    password: this.propPassword
+  })
+
+  submitted = false;
+
+  login() {
+    this.submitted = true
+    if (this.form.invalid) return;
+    console.log(this.form.value)
     // this.router.navigateByUrl('/');
   }
 }
